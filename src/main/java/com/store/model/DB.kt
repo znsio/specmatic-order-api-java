c4a32799043d0ef027fd8d22400c31b214b47d51
Naresh Jain <nashjain@gmail.com>
2023-11-29 17:38:54 +1100

    changed API user token from API-TOK,EN-HARI to API-TOKEN-SPEC

--- src/main/java/com/store/model/DB.kt
+++ src/main/java/com/store/model/DB.kt
@@ -3,7 +3,7 @@ package com.store.model
 object DB {
     private var PRODUCTS: MutableMap<Int, Product> = mutableMapOf(10 to Product("XYZ Phone", "gadget", 10, 10), 20 to Product("Gemini", "dog", 10, 20))
     private var ORDERS: MutableMap<Int, Order> = mutableMapOf(10 to Order(10, 2, "pending", 10), 20 to Order(10, 1, "pending", 20))
-    private val USERS: Map<String, User> = mapOf("API-TOKEN-HARI" to User("Hari"))
+    private val USERS: Map<String, User> = mapOf("API-TOKEN-SPEC" to User("Hari"))
 
     fun userCount(): Int {
         return USERS.values.count()
