3833ee14becab4f53042a52144462db9e0c2b901
Yogesh Ananda Nikam <yogeshnikam671@gmail.com>
2024-07-30 10:57:09 +0530

    Remove some endpoints from Products for demo purpose

--- src/main/java/com/store/controllers/Products.kt
+++ src/main/java/com/store/controllers/Products.kt
@@ -22,21 +22,6 @@ open class Products {
     @Autowired
     lateinit var productService: ProductService
 
-    @PostMapping("/products/{id}")
-    @Validated
-    fun update(
-        @PathVariable id: Int,
-        @Valid @RequestBody product: Product,
-        @AuthenticationPrincipal user: User
-    ): ResponseEntity<String> {
-        val productId = productService.addProduct(product.also {
-            if(product.type !in typesOfProducts)
-                throw ValidationException("type must be one of ${typesOfProducts.joinToString(", ")}")
-        })
-        productService.updateProduct(product)
-        return ResponseEntity(HttpStatus.OK)
-    }
-
     @GetMapping("/products/{id}")
     fun get(@PathVariable("id") id: Int): Product {
         try {
@@ -46,31 +31,9 @@ open class Products {
         }
     }
 
-    @PostMapping("/products")
-    fun create(@Valid @RequestBody newProduct: Product, @AuthenticationPrincipal user: User): ResponseEntity<Id> {
-        val productId = productService.addProduct(newProduct.also {
-            if(newProduct.type !in typesOfProducts)
-                throw ValidationException("type must be one of ${typesOfProducts.joinToString(", ")}")
-        })
-        return ResponseEntity(productId, HttpStatus.OK)
-    }
-
     @DeleteMapping("/products/{id}")
     fun delete(@PathVariable("id") id: Int, @AuthenticationPrincipal user: User): ResponseEntity<String> {
         productService.deleteProduct(id)
         return ResponseEntity(HttpStatus.OK)
-    }
-
-    @GetMapping("/products")
-    fun search(
-        @RequestParam(name = "name", required = false) name: String?,
-        @RequestParam(name = "type", required = false) type: String?,
-        @RequestParam(name = "status", required = false) status: String?
-    ): ResponseEntity<List<Product>> {
-        // An exception thrown by some internal bug...
-        if (name == "unknown")
-            return ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR)
-        val products = productService.findProducts(name, type, status)
-        return ResponseEntity(products, HttpStatus.OK)
     }
 }
